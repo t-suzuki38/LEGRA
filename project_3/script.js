7d86fd14a1017a8410c3961e0f6a783356e8159e
t-suzuki38 <157936069+t-suzuki38@users.noreply.github.com>
2026-09-22 20:39:50 +0900

    Delete project_3 directory

--- project_3/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const mainSearchBtn = document.getElementById('search-main-btn');
-  if(mainSearchBtn) {
-    mainSearchBtn.addEventListener('click', () => {
-      alert('位置情報を取得しました。周辺のカフェを検索します。');
-    });
-  }
-});
